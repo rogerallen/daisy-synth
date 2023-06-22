a1ffc03a9e1e8f26dfc292385bedc73365208342
Roger Allen <rallen@gmail.com>
2023-06-21 21:56:04 -0700

    keyboard work in progress

--- demo/demo.c
+++ demo/demo.c
@@ -58,19 +58,62 @@ static void init(void)
     state.cur_amplitude = 0.05;
 }
 
+// draw a proper keyboard 
+// https://bootcamp.uxdesign.cc/drawing-a-flat-piano-keyboard-in-illustrator-de07c74a64c6?gi=866815891348
+// a single octave is divided into seven diatonic steps and twelve chromatic steps. 
+// our range is 48/C3 to 76/E5.  But we will calculate things based on 3 octaves [48,84)
+// or 21 diatonic steps and 36 chromatic steps
 static void draw_key(int pitch, bool active)
 {
-    float cx = (pitch - 64.0) / 32.0;
-    float dx = 1.0 / 68.0;
-    int top_color = active ? 200 : 100;
-    int bot_color = active ? 255 : 128;
-    sgl_defaults();
-    sgl_begin_quads();
-    sgl_v2f_c3b(cx - dx, 0.5f, top_color, top_color, top_color);
-    sgl_v2f_c3b(cx - dx, -0.5f, bot_color, bot_color, bot_color);
-    sgl_v2f_c3b(cx + dx, -0.5f, bot_color, bot_color, bot_color);
-    sgl_v2f_c3b(cx + dx, 0.5f, top_color, top_color, top_color);
-    sgl_end();
+    // x ranges from -1 to 1
+    float margin_x = 0.15;
+    float start_x = -1 + margin_x;
+    float end_x = 1 - margin_x;
+    // 7*2+2 white keys fit between start_x & end_x
+    float white_dx = (end_x - start_x) / (7*2+2);
+    // 12 black keys fit within 7 white keys
+    float black_dx = 7*white_dx/12;
+    // 48                     60                     72                     84
+    // C                      C                      C                      C
+    // w,b,w,b,w,w,b,w,b,w,b, w,b,w,b,w,w,b,w,b,w,b, w,b,w,b,w,w,b,w,b,w,b, 
+    int white_key_idx_ary[] = {0,-1,1,-1,2,3,-1,4,-1,5,-1,6, 7+0,-1,7+1,-1,7+2,7+3,-1,7+4,-1,7+5,-1,7+6,  14+0,-1,14+1,-1,14+2,14+3,-1,14+4,-1,14+5,-1,14+6 };
+    int black_key_idx_ary[] = {-1,1,-1,3,-1,-1,6,-1,8,-1,10, -1,12+1,-1,12+3,-1,-1,12+6,-1,12+8,-1,12+10, -1,24+1,-1,24+3,-1,-1,24+6,-1,24+8,-1,24+10};
+    int p = pitch - 48;
+    assert(p < len(white_key_idx_ary));
+    assert(p >= 0);
+    int white_key_idx = white_key_idx_ary[p];
+    int black_key_idx = black_key_idx_ary[p];
+    printf("wki=%d bki=%d\n", white_key_idx, black_key_idx);
+    // one should be -1, the other should be 0 greater
+    assert(((white_key_idx >= 0) && (black_key_idx < 0)) ||
+           ((white_key_idx < 0) && (black_key_idx >= 0)));
+    if(white_key_idx >= 0) {
+        float x = start_x + white_key_idx*white_dx;
+        float dx = white_dx*0.9;
+        int top_color = active ? 200 : 100;
+        int bot_color = active ? 255 : 128;
+        sgl_defaults();
+        sgl_begin_quads();
+        sgl_v3f_c3b(x,      0.5f, 0.9f, top_color, top_color, top_color);
+        sgl_v3f_c3b(x,      0.0f, 0.9f, bot_color, bot_color, bot_color);
+        sgl_v3f_c3b(x + dx, 0.0f, 0.9f, bot_color, bot_color, bot_color);
+        sgl_v3f_c3b(x + dx, 0.5f, 0.9f, top_color, top_color, top_color);
+        sgl_end();
+    }
+    else {
+        float x = start_x + black_key_idx*black_dx;
+        float dx = black_dx*0.9;
+        int top_color = active ? 80 : 40;
+        int bot_color = active ? 100 : 50;
+        sgl_defaults();
+        sgl_begin_quads();
+        sgl_v3f_c3b(x,       0.2f, 0.5f, top_color, top_color, top_color);
+        sgl_v3f_c3b(x,      -0.5f, 0.5f, bot_color, bot_color, bot_color);
+        sgl_v3f_c3b(x + dx, -0.5f, 0.5f, bot_color, bot_color, bot_color);
+        sgl_v3f_c3b(x + dx,  0.2f, 0.5f, top_color, top_color, top_color);
+        sgl_end();
+    }
+
 }
 
 static void frame(void)
@@ -117,112 +160,109 @@ static void cleanup(void)
 }
 
 // return midi pitch for keyboard events.  return -1 for no match
+// range is 48/C3 to 76/E5
 static int key_code_to_pitch(sapp_keycode kc)
 {
     int pitch = -1;
     switch (kc) {
     case SAPP_KEYCODE_Z:
-        pitch = 12;
+        pitch = 48; // C3
         break;
     case SAPP_KEYCODE_S:
-        pitch = 13;
+        pitch = 49;
         break;
     case SAPP_KEYCODE_X:
-        pitch = 14;
+        pitch = 50;
         break;
     case SAPP_KEYCODE_D:
-        pitch = 15;
+        pitch = 51;
         break;
     case SAPP_KEYCODE_C:
-        pitch = 16;
+        pitch = 52;
         break;
     case SAPP_KEYCODE_V:
-        pitch = 17;
+        pitch = 53;
         break;
     case SAPP_KEYCODE_G:
-        pitch = 18;
+        pitch = 54;
         break;
     case SAPP_KEYCODE_B:
-        pitch = 19;
+        pitch = 55;
         break;
     case SAPP_KEYCODE_H:
-        pitch = 20;
+        pitch = 56;
         break;
     case SAPP_KEYCODE_N:
-        pitch = 21;
+        pitch = 57;
         break;
     case SAPP_KEYCODE_J:
-        pitch = 22;
+        pitch = 58;
         break;
     case SAPP_KEYCODE_M:
-        pitch = 23;
+        pitch = 59;
         break;
     case SAPP_KEYCODE_COMMA:
-        pitch = 24;
+        pitch = 60; // C4 = Middle C
         break;
     case SAPP_KEYCODE_L:
-        pitch = 25;
+        pitch = 61;
         break;
     case SAPP_KEYCODE_PERIOD:
-        pitch = 26;
+        pitch = 62;
         break;
 
         // upper keyboard
     case SAPP_KEYCODE_Q:
-        pitch = 24;
+        pitch = 60; // C4 = Middle C
         break;
     case SAPP_KEYCODE_2:
-        pitch = 25;
+        pitch = 61;
         break;
     case SAPP_KEYCODE_W:
-        pitch = 26;
+        pitch = 62;
         break;
     case SAPP_KEYCODE_3:
-        pitch = 27;
+        pitch = 63;
         break;
     case SAPP_KEYCODE_E:
-        pitch = 28;
+        pitch = 64;
         break;
     case SAPP_KEYCODE_R:
-        pitch = 29;
+        pitch = 65;
         break;
     case SAPP_KEYCODE_5:
-        pitch = 30;
+        pitch = 66;
         break;
     case SAPP_KEYCODE_T:
-        pitch = 31;
+        pitch = 67;
         break;
     case SAPP_KEYCODE_6:
-        pitch = 32;
+        pitch = 68;
         break;
     case SAPP_KEYCODE_Y:
-        pitch = 33;
+        pitch = 69;
         break;
     case SAPP_KEYCODE_7:
-        pitch = 34;
+        pitch = 70;
         break;
     case SAPP_KEYCODE_U:
-        pitch = 35;
+        pitch = 71;
         break;
     case SAPP_KEYCODE_I:
-        pitch = 36;
+        pitch = 72;  // C5
         break;
     case SAPP_KEYCODE_9:
-        pitch = 37;
+        pitch = 73;
         break;
     case SAPP_KEYCODE_O:
-        pitch = 38;
+        pitch = 74;
         break;
     case SAPP_KEYCODE_0:
-        pitch = 39;
+        pitch = 75;
         break;
     case SAPP_KEYCODE_P:
-        pitch = 40;
+        pitch = 76;  // E5
         break;
-    }
-    // shift 3 octaves up
-    if (pitch > 0) {
-        pitch += 3 * 12;
     }
     return pitch;
 }
